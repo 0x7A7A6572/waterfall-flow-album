d0a30575f1d0448a8edf82c1021d6b63aa62ef91
0x7A7A6572 <2623385294@qq.com>
2022-06-20 00:24:05 +0800

    base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div  >
+  <!-- <zx-image-grup-vue >
+  </zx-image-grup-vue> -->
+  <zx-water-fall :artssrc="onlySrc" :arts="arts.photography">
+  </zx-water-fall>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import zxImageGrupVue from './components/zx-image-grup.vue'
+import zxWaterFall from './components/zx-water-fall.vue'
 
+let titleArry = [];
+let summaryArray = [];
+let dataArray = [];
 export default {
   name: 'App',
+  data() {
+    return {
+      arts: {
+        "illustration": creatArtsName("illustration", 19, titleArry, summaryArray, dataArray),
+        "oil_painting": creatArtsName("oil_painting", 0, titleArry, summaryArray, dataArray),
+        "photography": creatArtsName("photography", 57, titleArry, summaryArray, dataArray),
+        "pixel_painting": creatArtsName("pixel_painting", 24, titleArry, summaryArray, dataArray),
+        "wall_painting": creatArtsName("wall_painting", 9, titleArry, summaryArray, dataArray)
+      },
+      onlySrc: creatArtsSrc("photography",57)
+    }
+  },
   components: {
-    HelloWorld
+    zxWaterFall,
   }
 }
+function creatArtsName(type, max, titleArry, summaryArray, dataArray) {
+    let arts = [];
+    if (titleArry != null && summaryArray != null && dataArray != null
+        && titleArry.length == summaryArray.length &&
+        summaryArray.length == dataArray.length && dataArray.length == max) {
+          console.log("come on!")
+    } else {
+        for (let i = 1; i <= max; i++) {
+            let artobj = {
+                src: "./art/" + type + "/" + type + i + ".png",
+                title: "",
+                summary: "",
+                date: ""
+            }
+            arts.push(artobj);
+        }
+    }
+    return arts;
+}
+
+function creatArtsSrc(type, max) {
+  let arts = [];
+    for (let i = 1; i <= max; i++) {
+      arts.push( "./art/" + type + "/" + type + i + ".png");
+    }
+  return arts;
+}
+
 </script>
 
 <style>
---
+++ src/components/zx-image-grup.vue
@@ -0,0 +1,52 @@
+<template>
+
+
+<div class="zx-water-fall">
+    <el-card v-for="item in arts" :key="item">
+        <el-image :src="item.src"
+            class="image" :preview-src-list="artssrc" 
+            loading="lazy"/>
+        <div style="padding: 14px">
+            <span>Delicious hamburger</span>
+            <div class="bottom card-header">
+                <div class="time">{{ currentDate }}</div>
+                <el-button text class="button">operation button</el-button>
+            </div>
+        </div>
+    </el-card>
+
+</div>
+
+
+</template>
+
+<script>
+import { ElImage } from 'element-plus'
+
+
+
+export default {
+    data() {
+        return {
+            loading: false,
+        }
+    },
+    props: {
+        arts: {
+            type: Object
+        },
+        artssrc: {
+            type: Object
+        }
+    },
+    components: {
+        // ElSkeleton,
+        ElImage
+    }
+}
+
+
+</script>
+
+<style>
+</style>
